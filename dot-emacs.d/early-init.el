01c72233fc7df2c412a3d175e7274e46b4800a93
Vivien Nepenthe <viv.nepenthe@gmail.com>
2024-07-09 16:12:56 -0700

    clean up early-init.el

--- dot-emacs.d/early-init.el
+++ dot-emacs.d/early-init.el
@@ -1,21 +1,17 @@
-;; Startup speed, annoyance suppression
-(setq gc-cons-threshold 10000000)
+(setq gc-cons-threshold 10000000) ; don't gc unless we're swapping
+;; annoyance suppression
 (setq byte-compile-warnings '(not obsolete))
 (setq warning-suppress-log-types '((comp) (bytecomp)))
 (setq native-comp-async-report-warnings-errors 'silent)
 (setq inhibit-startup-echo-area-message (user-login-name))
-
 ;; Default frame configuration: full screen, good-looking title bar on macOS
-(setq frame-resize-pixelwise t)
 (tool-bar-mode -1)
+(setq frame-resize-pixelwise t)
 (setq default-frame-alist '((fullscreen . maximized)
-
-                            ;; You can turn off scroll bars by uncommenting these lines:
                             (vertical-scroll-bars . nil)
                             (horizontal-scroll-bars . nil)
-
                             ;; Setting the face in here prevents flashes of
                             ;; color as the theme gets activated
                             (background-color . "#808080")
-                            (ns-appearance . dark)
-                            (ns-transparent-titlebar . t)))
+                            (ns-transparent-titlebar . t)
+			    ))
