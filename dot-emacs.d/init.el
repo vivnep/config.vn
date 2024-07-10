63ff59ee0961a5667028c4bd8da46b05a50cf27a
Vivien Nepenthe <viv.nepenthe@gmail.com>
2024-07-09 19:14:21 -0700

    Revert "don't hardcode emacs.d location"
    This reverts commit 1f5e7a35d8d9e7ce25b3a1cd0783fda71279c936.

--- dot-emacs.d/init.el
+++ dot-emacs.d/init.el
@@ -1,5 +1,5 @@
 (use-package org
   :config
   ;; encountered this https://raindev.io/blog/how-to-not-write-emacs-config-in-org/
-  (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
+  (org-babel-load-file "~/.emacs.d/config.org")
   )
