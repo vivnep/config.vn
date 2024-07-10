f2b96aa3e6ee3cc287638304466ad76135fb931e
Vivien Nepenthe <viv.nepenthe@gmail.com>
2024-07-09 20:02:16 -0700

    add swift and haskell grammars

--- dot-emacs.d/init.el
+++ dot-emacs.d/init.el
@@ -145,6 +145,7 @@ If the new path's directories does not exist, create them."
     (elisp "https://github.com/Wilfred/tree-sitter-elisp")
     (go "https://github.com/tree-sitter/tree-sitter-go")
     (go-mod "https://github.com/camdencheek/tree-sitter-go-mod")
+    (haskell "https://github.com/tree-sitter/tree-sitter-haskell")
     (html "https://github.com/tree-sitter/tree-sitter-html")
     (js . ("https://github.com/tree-sitter/tree-sitter-javascript" "master" "src"))
     (json "https://github.com/tree-sitter/tree-sitter-json")
@@ -154,6 +155,7 @@ If the new path's directories does not exist, create them."
     (python "https://github.com/tree-sitter/tree-sitter-python")
     (r "https://github.com/r-lib/tree-sitter-r")
     (rust "https://github.com/tree-sitter/tree-sitter-rust")
+    (swift "https://github.com/tree-sitter/tree-sitter-swift")
     (toml "https://github.com/tree-sitter/tree-sitter-toml")
     (tsx . ("https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src"))
     (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src"))
@@ -167,7 +169,6 @@ If the new path's directories does not exist, create them."
   (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t))
 ;; automatically use treesitter
 (use-package treesit-auto
-  
   :custom
   (treesit-auto-install 'prompt)
   :config
