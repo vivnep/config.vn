4b95a609e50e3b8c4c3c1565e03d97424ae62c9c
Vivien Nepenthe <viv.nepenthe@gmail.com>
2024-07-11 10:57:47 -0700

    Revert "add tabspaces"
    This reverts commit e97057000a93f3456e9a33c0c36da698d3875c81.

--- dot-emacs.d/init.el
+++ dot-emacs.d/init.el
@@ -227,22 +227,6 @@ If the new path's directories does not exist, create them."
 (use-package magit
   :ensure t)
 
-;; workspaces ux based on tab bar mode
-(use-package tabspaces
-  :hook (after-init . tabspaces-mode) ;; use this only if you want the minor-mode loaded at startup. 
-  :commands (tabspaces-switch-or-create-workspace
-             tabspaces-open-or-create-project-and-workspace)
-  :custom
-  (tabspaces-use-filtered-buffers-as-default t)
-  (tabspaces-default-tab "Default")
-  (tabspaces-remove-to-default t)
-  (tabspaces-include-buffers '("*scratch*"))
-  (tabspaces-initialize-project-with-todo t)
-  (tabspaces-todo-file-name "project-todo.org")
-  ;; sessions
-  (tabspaces-session t)
-  (tabspaces-session-auto-restore t))
-
 ;; persist history
 (use-package savehist
   :init
