7a59ddd0f0238a6ea23df070cee672d8d6185e62
Vivien Nepenthe <viv.nepenthe@gmail.com>
2024-07-11 13:03:32 -0700

    change repos to run before package initialize

--- dot-emacs.d/init.el
+++ dot-emacs.d/init.el
@@ -193,7 +193,7 @@ If the new path's directories does not exist, create them."
 ;;; packages
 ;; melpa and nongnu
 (use-package package
-  :config
+  :init
   (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
   (add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/") t)
   )
